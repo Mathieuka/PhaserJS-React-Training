4a7dddd73ddf1cd7dc437fb910c34cfc2a6128e4
MK <mathieu.kahlaoui.pro@gmail.com>
2022-04-26 16:08:42 +0800

    Restructuration

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -4,6 +4,61 @@ import Sprite = Phaser.Physics.Arcade.Sprite;
 
 let player:  Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
 
+function createPlatforms (this_: any) {
+  const platforms = this_.physics.add.staticGroup();
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
+  return platforms
+}
+
+const createPlayer = (this_: any, platforms: any) => {
+  player = this_.physics.add.sprite(100, 450, 'dude');
+  player.setBounce(0.3);
+  player.setCollideWorldBounds(true);
+  this_.physics.add.collider(platforms, player)
+
+  this_.anims.create({
+    key: 'left',
+    frames: this_.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this_.anims.create({
+    key: 'turn',
+    frames: [{ key: 'dude', frame: 4 }],
+    frameRate: 20,
+  });
+
+  this_.anims.create({
+    key: 'right',
+    frames: this_.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+}
+
+const createStars = (this_: any, platforms: any) => {
+  const stars = this_.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 }
+  });
+  stars.children.iterate(function (child: GameObject) {
+    (child as (GameObject & Sprite)).setBounceY(Phaser.Math.FloatBetween(0.2, 0.5))
+  });
+
+  this_.physics.add.collider(platforms, stars)
+
+  const collectStar =  (player:  Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, star: GameObject & Sprite) => {
+    star.disableBody(true, true);
+  }
+
+  this_.physics.add.overlap(player, stars, collectStar as any, undefined, this);
+}
+
 export default class HelloWorldScene extends Phaser.Scene {
   constructor() {
     super('helloworld')
@@ -25,55 +80,13 @@ export default class HelloWorldScene extends Phaser.Scene {
     this.add.image(400, 300, 'sky');
 
     // Static platforms group
-    const platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+    const platforms = createPlatforms(this)
 
-    // Static Stars group
-    const stars = this.physics.add.group({
-      key: 'star',
-      repeat: 11,
-      setXY: { x: 12, y: 0, stepX: 70 }
-    });
-    stars.children.iterate(function (child: GameObject) {
-      (child as (GameObject & Sprite)).setBounceY(Phaser.Math.FloatBetween(0.2, 0.5))
-    });
+    // Dynamic player sprite
+    createPlayer(this, platforms)
 
-    this.physics.add.collider(platforms, stars)
-
-    const collectStar =  (player:  Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, star: GameObject & Sprite) => {
-      star.disableBody(true, true);
-    }
-
-    this.physics.add.overlap(player, stars, collectStar as any, undefined, this);
-
-    // Player sprite
-    player = this.physics.add.sprite(100, 450, 'dude');
-    player.setBounce(0.3);
-    player.setCollideWorldBounds(true);
-    this.physics.add.collider(platforms, player)
-
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
+    // Dynamic stars group
+    createStars(this, platforms)
   }
 
   update(time: number, delta: number) {
